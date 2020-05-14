9a717c0d2cf789670234a30ab2ab5c07b7e8f1f1
WojTechM <wojciech.m1997@gmail.com>
2020-05-14 17:42:51 +0200

    Zadanie1: usunięcie części praktycznej w Javie 4, usunięcie nadmiarowego tekstu

--- src/main/java/warsztatywprowadzajace/Zadanie1.java
+++ src/main/java/warsztatywprowadzajace/Zadanie1.java
@@ -5,13 +5,15 @@ import java.util.List;
 /**
  * <h1>Czasy przed generykami</h1>
  *
+ * Uwaga - IntelliJ bardzo nie lubi nieużywania generyków, ani języka polskiego. Aby ograniczyć ilość
+ * podkreśleń/zakolorowań, będziesz musiał ograniczyć inspekcje. Aby to zrobić kliknij w dolnym prawym rogu
+ * (praktycznie na samym rogu okna IntelliJa) w ikonkę inspektora (taka buźka) lub użyj kombinacji klawiszy
+ * Ctrl + Shift + Alt + 'H', a następnie ogranicz inspekcje do poziomu 'syntax'.
+ *
  * <ol>
- *     <li>Stwórz nowy projekt w którym będziesz pracować na javie 4. #mantra</li>
- *     <li>Skopiuj i wklej tam metodę `róbRzeczyZListą()`.</li>
- *     <li>Zaimplementuj ją, i zobacz, jak straszne były czasy javy 4.</li>
- *     <li>Zaimplementowaną metodę wklej tutaj - jeśli nic nie skopałeś, to powinna się kompilować!</li>
- *     <li>Zaimplementuj metodę `róbRzeczyZGenerycznąListą()`</li>
- *     <li>Porównaj implementacje tych metod, a następnie przeczytaj podsumowanie na dole.</li>
+ *     <li>Przeczytaj uważnie implementację metody `retroJavaStyle`.</li>
+ *     <li>Wykonaj kroki wypisane w metodzie `modernJavaStyle`</li>
+ *     <li>Porównaj implementacje obu metod, a następnie przeczytaj podsumowanie na dole.</li>
  * </ol>
  *
  * @author Wojciech Makiela
@@ -19,89 +21,80 @@ import java.util.List;
 public class Zadanie1 {
 
     public static void main(String[] args) {
-        róbRzeczyZListą();
-        róbRzeczyZGenerycznąListą();
+        retroJavaStyle();
+        modernJavaStyle();
+        howDoesItWork();
     }
 
-    static void róbRzeczyZListą() {
-        // Stwórz nową listę o nazwie 'strings'
+    static void retroJavaStyle() {
+        // Stworzenie nowej listy
+        List strings = new ArrayList();
+        // Dodanie elementów: 'pierwszy' i 'drugi'
+        strings.add("pierwszy");
+        strings.add("drugi");
 
-        // Dodaj do niej elementy: 'pierwszy' i 'drugi'
-
-
-        // Bądź niegrzecznym chłopcem i dodaj do listy 'new Integer(3)'
+        // Dodanie elementów: 3
         // Autoboxing pojawił się w Javie 5, więc trzeba ręcznie zapakować '3' w 'new Integer'
+        strings.add(new Integer(3));
 
-        // Wygląda to źle, a IntelliJ nie pokazuje błędu...
-        // Przypał wisi w powietrzu, ale kontynuuj
-
-        // Wyciągnij pierwszy element z listy, i przypisz go do zmiennej typu 'String'
-        String pierwszy; // = strings.get(0);
-
-        // No i przypał. Błąd kompilacji. IntelliJ świeci czerwienią. W tle słychać syreny.
+        // Wyciągnięcie pierwszego elementu z listy
         // Przed wprowadzeniem generyków wszystkie kolekcje były kolekcjami obiektów.
-        // Co za tym idzie - by przypisać nasz pierwszy element do zmiennej typu 'String' trzeba
-        // użyć rzutowania! Dodaj je teraz.
+        // Co za tym idzie - by przypisać pierwszy element do zmiennej typu 'String' trzeba
+        // użyć rzutowania.
+        String string = (String) strings.get(0);
 
-
-        // No to co. Teraz zrób to samo dla ostatniego elementu (naszego Integera).
-        String ostatni; // (String) strings.get(2);
-
-        // Po odpaleniu metody powinien pojawić się wyjątek ClassCastException.
-        // Robota w Javie 4 skończona. Wróć do warsztatowego repo.
+        // Wyciągnięcie trzeciego elementu z listy
+        // Z racji tego, że nie mamy ograniczenia co może trafić do listy, możemy omyłkowo doprowadzić do
+        // nielegalnego rzutowania.
+        try {
+            String anInteger = (String) strings.get(2);
+        } catch (ClassCastException e) {
+            System.out.println("Wyjątek czasu wykonania - nielegalne rzutowanie.");
+        }
     }
 
-    static void róbRzeczyZGenerycznąListą() {
+    static void modernJavaStyle() {
         // Stwórz listę parametryzowaną typem 'String' i nazwij ją 'strings'
 
-        // Dodaj do niej elementy: 'pierwszy' i 'drugi'
+
+        // Dodaj elementy: 'pierwszy' i 'drugi'
 
 
-        // Bądź niegrzecznym chłopcem i dodaj do listy 'new Integer(3)'
+        // Dodaj elementy: 3
+        // Podjęcie się takiej próby poskutkuje błędem kompilacji. Przeczytaj treść i zakomentuj problematyczną linijkę.
 
-        // Hah! Compile error!
-        // Zakomentuj tą linijkę tak, żeby dalej się kompilowało.
 
         // Wyciągnij pierwszy element z listy, i przypisz go do zmiennej typu 'String'
-        String pierwszy; // = strings.get(0);
 
-        // Uruchom metodę.
+
     }
 
     /**
      * <h1>Podsumowanie:</h1>
      * <p>
-     * Powinieneś teraz mieć 2 podobne implementacje. Jedną z użyciem generyków, a drugą w stylu retro.
-     * Jeśli nie mogłeś napisać kodu w Javie 4 (z powodu przyczyn jak mniemam),
-     * to przeklej tutaj gotowca dostępnego w katalogu resources w pliku 'RetroJava'.
-     * </p>
+     * Powinieneś teraz mieć 2 podobne implementacje - bez oraz z wykorzystaniem generyków.
      * <p>
      * Jak już wiesz, przed Javą 1.5 wszystkie kolekcje zawierały obiekty (Object).
-     * Jako że wszystko w Javie jest obiektem, lub ma obiektowy odpowiednik (siema prymitywy), to
-     * kolekcje były reużywalne.
-     * Problem był jednak taki, że nie mogliśmy narzucać ograniczeń na to, co trafia do naszej kolekcji,
-     * co mogło być źródłem wyjątków podczas wyciągania jej elementów.
+     * Jako że wszystko w Javie da się przedstawić jako obiekt, to kolekcje były reużywalne.
+     * Problemem był jednak brak ograniczeń tego, co trafia do kolekcji. Mogło być to źródłem wyjątków podczas
+     * pobierania jej elementów.
      * </p>
-     * I wtedy weszły one - Generyki. Bohaterowie na których zasługiwaliśmy.
-     * Generyki pozwalają nam ograniczać typy elementów dodawanych do kolekcji, jak i dają znać kompilatorowi
-     * jak dodać rzutowanie - czytaj dalej.
+     * Generyki powstały jako rozwiązanie tego problemu. Pozwalają nam ograniczać typ elementów dodawanych do
+     * kolekcji oraz dają znać kompilatorowi jakie dodać rzutowanie.
      */
-    private static void howItWorks() {
-        // <String> mówi kompilatorowi, że w naszej liście będą tylko instancje klasy 'String'
+    private static void howDoesItWork() {
+        // Sygnatura `<String>` dostarcza kompilatorowi informacji, że w naszej liście będą tylko instancje klasy
+        // 'String'
         List<String> strings = new ArrayList<>();
 
-        // Kompilator uwzględnia tę informację i dokonuje sprawdzenia, czy elementy wkładane
-        // do kolekcji są odpowiedniego typu.
+        // Kompilator uwzględnia tę informację i dokonuje sprawdzenia, czy elementy wkładane do kolekcji są
+        // odpowiedniego typu.
         strings.add("pierwszy");
-//        strings.add(new Integer(3)); // Błąd kompilacji
+        // strings.add(new Integer(3)); // Błąd kompilacji
 
-        // Wyciąganie elemetów przed Javą 1.5 wymagało ręcznego rzutowania.
-        // Aktualnie kompilator sam dodaje odpowiednie rzutowanie w czasie kompilacji.
-        // Finalnie bytecode wygenerowany podczas kompilacji zawiera informacje o rzutowaniach,
-        // mimo tego, że nie ma ich w kodzie. Sprawia to, że kod źródłowy jest czystszy i łatwiejszy do zrozumienia.
-        String first = strings.get(0);
-
-        // Ale skoro lista ma 'Stringi', to po co tu rzutowanie? Coś mi tu śmierdzi...
-        // O tym później. Najpierw trochę koderki z generykami, aby utrwalić zdobytą wiedzę.
+        // Wyciąganie elemetów przed Javą 1.5 wymagało ręcznego rzutowania. Po wprowadzeniu generyków rzutowanie to
+        // dodawane jest przez sam kompilator. Innymi słowy - rzutowanie dalej istnieje na poziomie kodu bajtowego,
+        // ale w samym kodzie javovym nie jest ono potrzebne.
+        String string = strings.get(0);
     }
 }
